5f2253ab66d77c4e804b0996223fc89504f5bfb2
spinoandraptos <spinoandraptos@Ubuntu-22.04.myguest.virtualbox.org>
2025-01-14 06:40:52 -0700

    Updated dashboard page

--- hack4good/src/components/sidebar.tsx
+++ hack4good/src/components/sidebar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 bg-teal-500 text-white h-screen p-4">
+    <aside className="w-64 bg-teal-500 text-white h-screen p-4 sticky  top-0">
       <div className="text-center mb-6">
         <img src={'https://mwh.muhammadiyah.org.sg/wp-content/uploads/2021/06/mwh-esig-footer-062.png'}
                 className="object-cover rounded-md mb-4"
@@ -10,7 +10,7 @@ export default function Sidebar() {
       </div>
       <nav>
         <ul>
-          <li><Link href="/" className="block py-2 hover:bg-teal-600 px-4">Home</Link></li>
+          <li><Link href="/" className="block py-2 hover:bg-teal-600 px-4">Dashboard</Link></li>
           <li><Link href="/transactions" className="block py-2 hover:bg-teal-600 px-4">Transactions</Link></li>
           <li><Link href="/profile" className="block py-2 hover:bg-teal-600 px-4">Profile</Link></li>
         </ul>
