49c2812f3def1b671e0c21b0905c65605572e35c
Thunderdragon221 <arceus2585@gmail.com>
2025-01-16 21:22:45 +0800

    form functions for 2 admin pages

--- hack4good/src/app/pending_password_resets/page.tsx
+++ hack4good/src/app/pending_password_resets/page.tsx
@@ -3,56 +3,57 @@
 import Sidebar from '../../components/sidebar'
 import Link from 'next/link'
 import { useState } from 'react'
-import Transactions from '../transactions/page'
+import { useRouter } from 'next/navigation'
 
 type Request = {
-    requestId: number
+    requestId: string
     residentId: string
     residentName: string
     residentImg: string
 }
 
 interface FormData {
-  quantity: number
-  item: string
+  requestId: string
+  residentId: string
+  newPassword: string
 }
 
 const requests: Request[] = [
-    { requestId: 1, residentId: "a1", residentName: "Adam", residentImg: `https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/24544933-ca76-403b-b102-56b18c235d74/ba8e5245-deaf-4acb-af8d-0ab1ba2d42a8.png` },
-    { requestId: 2, residentId: "a2", residentName: "", residentImg: `https://via.placeholder.com/200` } ,
-    { requestId: 3, residentId: "a3", residentName: "", residentImg: `https://via.placeholder.com/200` },
-    { requestId: 4, residentId: "a4", residentName: "", residentImg: `https://via.placeholder.com/200` },
-    { requestId: 5, residentId: "a5", residentName: "", residentImg: `https://via.placeholder.com/200` },
-    { requestId: 6, residentId: "a6", residentName: "", residentImg: `https://via.placeholder.com/200` },
-    { requestId: 7, residentId: "a7", residentName: "", residentImg: `https://via.placeholder.com/200` },
+    { requestId: "1", residentId: "a1", residentName: "Adam", residentImg: `https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/24544933-ca76-403b-b102-56b18c235d74/ba8e5245-deaf-4acb-af8d-0ab1ba2d42a8.png` },
+    { requestId: "2", residentId: "a2", residentName: "", residentImg: `https://via.placeholder.com/200` } ,
+    { requestId: "3", residentId: "a3", residentName: "", residentImg: `https://via.placeholder.com/200` },
+    { requestId: "4", residentId: "a4", residentName: "", residentImg: `https://via.placeholder.com/200` },
+    { requestId: "5", residentId: "a5", residentName: "", residentImg: `https://via.placeholder.com/200` },
+    { requestId: "6", residentId: "a6", residentName: "", residentImg: `https://via.placeholder.com/200` },
+    { requestId: "7", residentId: "a7", residentName: "", residentImg: `https://via.placeholder.com/200` },
   ]
 
 export default function Home() {
 
-  const [reqFormData, setReqFormData] = useState({
-    1: { id:'', name:'', password: '' },
-    2: { id:'', name:'', password: '' },
-    3: { id:'', name:'', password: '' },
-    4: { id:'', name:'', password: '' },
-    5: { id:'', name:'', password: '' },
-    6: { id:'', name:'', password: '' },
-    7: { id:'', name:'', password: '' },
-  });
+  const router = useRouter()
+    const [reqFormData, setReqFormData] = useState(
+      requests.map((request) => ({
+        requestId: "",
+        residentId: "",
+        password: ""
+      }))
+    );
+    
+    const handleReqChange = (e: React.ChangeEvent<HTMLInputElement>, index: number, requestID: string, residentID: string) => {
+      const { name, value  } = e.target
+      const updatedReqFormData = [...reqFormData]
+      updatedReqFormData[index] = { ...updatedReqFormData[index], requestId: requestID, residentId: residentID, password: value }
+      setReqFormData(updatedReqFormData)
+    };
 
-  const handleChange = (id: number, product:string, value: string) => {
-    setReqFormData((prev) => ({
-      ...prev,
-      [id]: { ...prev[id], id: product, quantity: value },
-    }));
-  };
-
-  const handleSubmit = (id: number) => {
-    // Reset form data for that item after submission
-    setReqFormData((prev) => ({
-      ...prev,
-      [id]: { id:'', quantity: ''},
-    }));
-  };
+    const handleRequestSubmit = ( e: React.FormEvent, index: number ) => {
+      e.preventDefault()
+      const reqToSubmit = reqFormData[index]  
+      alert(`Password has been reset! Password: ${reqToSubmit.password}`)
+      const updatedReqFormData = [...reqFormData]
+      updatedReqFormData[index] = { ...updatedReqFormData[index], requestId: '', residentId: '', password: '' }
+      setReqFormData(updatedReqFormData)
+    };
 
   return (
     <div className="flex">
@@ -67,7 +68,7 @@ export default function Home() {
         </header>
     <h1 className="text-3xl font-bold text-teal-700 mb-8">Resident password reset requests pending approval</h1>
     <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8">
-          {requests.map((request) => (
+          {requests.map((request, index) => (
             <div key={request.requestId} className="bg-white p-4 rounded-lg shadow-md">
               <img
                 src={request.residentImg}
@@ -75,7 +76,7 @@ export default function Home() {
               />
               <p className="text-teal-500">Resident name: {request.residentName}</p>
               <p className="text-teal-500">Resident user ID: {request.residentId}</p>
-              <form onSubmit=   {handleSubmit} className="space-y-4">
+              <form onSubmit={(e)=>{handleRequestSubmit(e, index)}} className="space-y-4">
               <div>
                 <input
                     type="text"
@@ -83,8 +84,8 @@ export default function Home() {
                     name={request.residentName}
                     className="mt-1 p-2 w-full border rounded-md focus:ring-2 focus:ring-blue-500"
                     placeholder="Enter new password"
-                    value={reqFormData[request.requestId].password}
-                    onChange={handleChange}
+                    value={reqFormData[index].password}
+                    onChange={(e) => {handleReqChange(e, index, request.requestId, request.residentId)}}
                     required
                 />
               </div>
